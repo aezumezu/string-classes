003740fd625e195ac2cc116e7e508fb752d3fad9
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-09-09 12:45:16 +0100

    test Spec frame setup

---
+++ spec/string-classesSpec.js
@@ -0,0 +1,105 @@
+describe('String class tests.', function() {
+  'use strict';
+  describe('hasVowel method', function() {
+
+    it('returns true if the string contains vowel.', function() {
+      
+    });
+
+    it('returns false if the string contains no vowel.', function() {
+      
+    });
+
+    it('throws an error if input is not a string.', function() {
+      
+    });
+  });
+
+  describe('toUpper method', function() {
+
+    it('returns the upper case equivalent of a given string.', function() {
+      
+    });
+
+    it('returns uppercase string where applicable.', function() {
+      
+    });
+  });
+
+  describe('toLower method', function() {
+  
+    it('returns the lower case equivalent of a string.', function() {
+      
+    });
+  
+    it('returns lower case string where applicable.', function() {
+      
+    });
+  });
+
+  describe('ucFirst method', function() {
+  
+    it('converts the first character of a string to upper case.', function(){
+      
+    });
+  
+    it('returns a string as is if the first letter is upper  case.', function() {
+      
+    });
+    
+    it('returns the string as is if first character is not a letter.', function() {
+      
+    });
+  });
+  
+  describe('isQuestion method', function() {
+  
+    it('returns true is the string ends with a \'?\' mark.', function() {
+      
+    });
+  
+    it('returns false if the string does not end with a \'?\' mark.', function() {
+      
+    });
+  });
+
+  describe('words method', function() {
+  
+    it('returns an array of words in the string.', function() {
+      
+    });
+  
+    it('ensures array content are not empty.', function() {
+      
+    });
+  });
+  
+  describe('wordCount method', function() {
+  
+    it('counts the words in a string and returns the number.', function() {
+      
+    });
+  });
+
+  describe('toCurrency method', function() {
+    
+    it('confirms that input is a number-like string.', function() {
+      
+    });
+  
+    it('returns a currency representation of the number.', function() {
+      
+    });
+  });
+  
+  describe('fromCurrency method', function() {
+  
+    it('confirms that input is a number-like string.', function() {
+      
+    });
+  
+    it('returns a number representation of the currency string.', function() {
+      
+    });
+  });
+});
